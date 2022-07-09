9d9dec0b013f581ff29fa584807acf8dc1837a56
valzgit <nikola.valzorio.1998@gmail.com>
2022-07-09 17:03:41 +0200

    Add pretty printing of the general info about the car

--- main.py
+++ main.py
@@ -14,4 +14,12 @@ for car in cars:
     link = car.find('a')['href']
     car_specific_html = session.get(url+link,headers={'User-Agent': 'Mozilla/5.0'}, allow_redirects=True)
     car_page_soup = BeautifulSoup(car_specific_html.text,'lxml')
-    print(car_page_soup.prettify())
+
+    allCarInfo = car_page_soup.find('div', class_='classified-content')
+    realGeneralInfo = allCarInfo.find('section', class_='js_fixedContetLoad').find('div', class_='infoBox').find('div', class_='uk-grid')
+    allInfoRows = realGeneralInfo.findAll('div', class_='divider')
+    for infoRow in allInfoRows:
+        infoClass = infoRow.find('div', class_='uk-width-1-2')
+        infoValue = infoRow.find('div', class_='uk-width-1-2 uk-text-bold')
+        print(infoClass.text + " - " + infoValue.text)
+    print("-------------------------------------------------------")
