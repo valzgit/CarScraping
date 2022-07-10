be1351a763eb31c2a9b078d174fcdfeda1fea03f
valzgit <nikola.valzorio.1998@gmail.com>
2022-07-11 00:00:34 +0200

    Finish scraper

--- main.py
+++ main.py
@@ -39,62 +39,65 @@ while fetched_cars < 22000:
         else:
             set.add(link)
             print("Going to link: " + link)
-        car_specific_html = session.get(link ,headers={'User-Agent': 'Mozilla/5.0'}, allow_redirects=True)
-        car_page_soup = BeautifulSoup(car_specific_html.text,'lxml')
-
-        carPrice = car_page_soup.find('span', class_='priceClassified regularPriceColor')
-
-        if carPrice is None:
-            carPrice = car_page_soup.find('span', class_='priceClassified discountedPriceColor')
-        if carPrice.text == "Po dogovoru":
-            continue
         try:
+            car_specific_html = session.get(link ,headers={'User-Agent': 'Mozilla/5.0'}, allow_redirects=True)
+            car_page_soup = BeautifulSoup(car_specific_html.text,'lxml')
+
+            carPrice = car_page_soup.find('span', class_='priceClassified regularPriceColor')
+
+            if carPrice is None:
+                carPrice = car_page_soup.find('span', class_='priceClassified discountedPriceColor')
+            if carPrice.text == "Po dogovoru":
+                continue
+
             cena = int(carPrice.text.split()[0].replace('.', ''))
+
+
+            sellingPlace = car_page_soup.find('div', class_='infoBox js-tutorial-contact').find('div', class_='uk-grid uk-margin-top-remove').find('div',class_='uk-width-1-2')
+            grad = sellingPlace.contents[0].strip()
+
+            allCarInfo = car_page_soup.find('div', class_='classified-content')
+            generalInfo = allCarInfo.find('section', class_='js_fixedContetLoad').find('div', class_='infoBox').find('div', class_='uk-grid')
+            allInfoRows = generalInfo.findAll('div', class_='divider')
+            for infoRow in allInfoRows:
+                infoClass = infoRow.find('div', class_='uk-width-1-2')
+                infoValue = infoRow.find('div', class_='uk-width-1-2 uk-text-bold')
+                if infoClass.text == "Stanje:":
+                    stanje = infoValue.text
+                elif infoClass.text == "Marka":
+                    marka = infoValue.text
+                elif infoClass.text == "Model":
+                    model = infoValue.text
+                elif infoClass.text == "Karoserija":
+                    karoserija = infoValue.text
+                elif infoClass.text == "Gorivo":
+                    vrsta_goriva = infoValue.text
+                elif infoClass.text == "Godište":
+                    godiste = int(infoValue.text.replace('.', ''))
+                elif infoClass.text == "Kilometraža":
+                    kilometraza = int(infoValue.text.split()[0].replace('.', ''))
+                elif infoClass.text == "Kubikaža":
+                    kubikaza = int(infoValue.text.split()[0])
+                elif infoClass.text == "Snaga motora":
+                    snaga_motora = int(infoValue.text.split()[0].split("/")[0])
+
+            additionalInfo = allCarInfo.find('div', class_='js-tab-classified-content classified-content').find('div', class_='infoBox').find('div', class_='uk-grid js-hidden uk-margin-top')
+            additionalInfoRows = additionalInfo.findAll('div', class_='divider')
+            for additionalInfoRow in additionalInfoRows:
+                infoClass = additionalInfoRow.find('div', class_='uk-width-1-2')
+                infoValue = additionalInfoRow.find('div', class_='uk-width-1-2 uk-text-bold')
+                if infoClass.text == "Boja":
+                    boja = infoValue.text
+                elif infoClass.text == "Menjač":
+                    menjac = infoValue.text
+                elif infoClass.text == "Broj vrata":
+                    broj_vrata = int(infoValue.text.split()[0].split("/")[0])
+            database.insertCar(marka,model,cena,stanje,grad,godiste,karoserija,vrsta_goriva,boja,kubikaza,snaga_motora, kilometraza, menjac, broj_vrata)
+            fetched_cars += 1
+            print("-------------------------"+str(fetched_cars)+"------------------------------")
         except Exception as error:
+            print(error)
             continue
-
-        sellingPlace = car_page_soup.find('div', class_='infoBox js-tutorial-contact').find('div', class_='uk-grid uk-margin-top-remove').find('div',class_='uk-width-1-2')
-        grad = sellingPlace.contents[0].strip()
-
-        allCarInfo = car_page_soup.find('div', class_='classified-content')
-        generalInfo = allCarInfo.find('section', class_='js_fixedContetLoad').find('div', class_='infoBox').find('div', class_='uk-grid')
-        allInfoRows = generalInfo.findAll('div', class_='divider')
-        for infoRow in allInfoRows:
-            infoClass = infoRow.find('div', class_='uk-width-1-2')
-            infoValue = infoRow.find('div', class_='uk-width-1-2 uk-text-bold')
-            if infoClass.text == "Stanje:":
-                stanje = infoValue.text
-            elif infoClass.text == "Marka":
-                marka = infoValue.text
-            elif infoClass.text == "Model":
-                model = infoValue.text
-            elif infoClass.text == "Karoserija":
-                karoserija = infoValue.text
-            elif infoClass.text == "Gorivo":
-                vrsta_goriva = infoValue.text
-            elif infoClass.text == "Godište":
-                godiste = int(infoValue.text.replace('.', ''))
-            elif infoClass.text == "Kilometraža":
-                kilometraza = int(infoValue.text.split()[0].replace('.', ''))
-            elif infoClass.text == "Kubikaža":
-                kubikaza = int(infoValue.text.split()[0])
-            elif infoClass.text == "Snaga motora":
-                snaga_motora = int(infoValue.text.split()[0].split("/")[0])
-
-        additionalInfo = allCarInfo.find('div', class_='js-tab-classified-content classified-content').find('div', class_='infoBox').find('div', class_='uk-grid js-hidden uk-margin-top')
-        additionalInfoRows = additionalInfo.findAll('div', class_='divider')
-        for additionalInfoRow in additionalInfoRows:
-            infoClass = additionalInfoRow.find('div', class_='uk-width-1-2')
-            infoValue = additionalInfoRow.find('div', class_='uk-width-1-2 uk-text-bold')
-            if infoClass.text == "Boja":
-                boja = infoValue.text
-            elif infoClass.text == "Menjač":
-                menjac = infoValue.text
-            elif infoClass.text == "Broj vrata":
-                broj_vrata = int(infoValue.text.split()[0].split("/")[0])
-        database.insertCar(marka,model,cena,stanje,grad,godiste,karoserija,vrsta_goriva,boja,kubikaza,snaga_motora, kilometraza, menjac, broj_vrata)
-        fetched_cars += 1
-        print("-------------------------"+str(fetched_cars)+"------------------------------")
     page += 1
     print("\n\nFETCHING PAGE NUMBER " + str(page) + "...............\n")
 database.closeConnection()
