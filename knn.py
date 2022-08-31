0ea2c823a031f441cbe84c546db5b63ca4df6ea5
valzgit <nikola.valzorio.1998@gmail.com>
2022-08-31 21:31:39 +0200

    Test with multiple k values

--- knn.py
+++ knn.py
@@ -20,61 +20,74 @@ cars = Car.removeAllCarsThatHaveOutOfRangeNumericalParams(cars)
 print(len(cars))
 Car.shuffle(cars)
 Car.calculateNormalizationValues(cars)
-train_cars, test_cars = Car.separateIntoTrainAndTestData(cars, 99)
+train_cars, test_cars = Car.separateIntoTrainAndTestData(cars, 99.94)
 guessed_bucket_classes = ["guessed [<= 2000]", "guessed [<= 4999]", "guessed [<= 9999]", "guessed [<= 14999]",
                           "guessed [<= 19999]", "guessed [<= 24999]", "guessed [<= 29999]", "guessed [> 30000]"]
 bucket_classes = ["<= 2000", "<= 4999", "<= 9999", "<= 14999", "<= 19999", "<= 24999", "<= 29999", "> 30000"]
-bucket_matrix = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]]
 initial_k = round(np.sqrt(len(train_cars)))
+min_k = round(9 / 10 * initial_k)
+max_k = round(11 / 10 * initial_k)
+print("Testing k values from " + str(min_k) + " to " + str(max_k))
+while min_k < max_k:
+    bucket_matrix = [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0]]
+    for test_car in test_cars:
+        minHeap = []
+        train_car_iterator = 0
+        while train_car_iterator < len(train_cars):
+            train_car = train_cars[train_car_iterator]
+            distance = Car.calculateEuclideanDistance(test_car, train_car)
+            minHeap.append([distance, train_car_iterator, train_car])
+            train_car_iterator += 1
 
-for test_car in test_cars:
-    minHeap = []
-    train_car_iterator = 0
-    while train_car_iterator < len(train_cars):
-        train_car = train_cars[train_car_iterator]
-        distance = Car.calculateEuclideanDistance(test_car, train_car)
-        minHeap.append([distance, train_car_iterator, train_car])
-        train_car_iterator += 1
+        heapq.heapify(minHeap)
+        buckets = [0, 0, 0, 0, 0, 0, 0, 0]
+        while_counter = 0
+        while while_counter < initial_k:
+            distance, unimportant, car = minHeap[while_counter]
+            Car.enrichPriceBucket(car, buckets)
+            while_counter += 1
 
-    heapq.heapify(minHeap)
-    buckets = [0, 0, 0, 0, 0, 0, 0, 0]
-    while_counter = 0
-    while while_counter < initial_k:
-        distance, unimportant, car = minHeap[while_counter]
-        Car.enrichPriceBucket(car, buckets)
-        while_counter += 1
+        maximum = 0
+        max_position = -1
+        counter = 0
+        while counter < len(buckets):
+            if buckets[counter] > maximum:
+                maximum = buckets[counter]
+                max_position = counter
+            counter += 1
 
-    maximum = 0
-    max_position = -1
-    counter = 0
-    while counter < len(buckets):
-        if buckets[counter] > maximum:
-            maximum = buckets[counter]
-            max_position = counter
-        counter += 1
+        bucket_matrix[max_position][test_car.getPriceBucketIndex()] += 1
 
-    bucket_matrix[max_position][test_car.getPriceBucketIndex()] += 1
+        print("Guessed [" + bucket_classes[max_position] + "]" + " and real was [" + bucket_classes[
+            test_car.getPriceBucketIndex()] + "]")
+        # print(pd.DataFrame(bucket_matrix, columns=bucket_classes, index = guessed_bucket_classes).to_string())
+    print(pd.DataFrame(bucket_matrix, columns=bucket_classes, index=guessed_bucket_classes).to_string())
 
-    # print("Guessed [" + bucket_classes[max_position]+"]" + " and real was [" + bucket_classes[test_car.getPriceBucketIndex()] + "]")
-    # print(pd.DataFrame(bucket_matrix, columns=bucket_classes, index = guessed_bucket_classes).to_string())
-print(pd.DataFrame(bucket_matrix, columns=bucket_classes, index=guessed_bucket_classes).to_string())
+    memorized_accuracies = []
+    top_A = 0
+    bottom_A = 0
+    i = 0
+    j = 0
 
-memorized_accuracies = []
-top_A = 0
-bottom_A = 0
-i = 0
-j = 0
-while i < len(bucket_matrix):
-    while j < len(bucket_matrix[0]):
-        if i == j:
-            top_A += bucket_matrix[i][j]
-        bottom_A += bucket_matrix[i][j]
-        j += 1
-    i += 1
-A = top_A / bottom_A
-print("Accuracy is [A = " + str(A) + "]")
-memorized_accuracies.append([initial_k, A])
+    while i < 8:
+        while j < 8:
+            if i == j:
+                top_A += bucket_matrix[i][j]
+            bottom_A += bucket_matrix[i][j]
+            j += 1
+        j = 0
+        i += 1
+    print("Top A = " + str(top_A))
+    print("Bottom A = " + str(bottom_A))
+    A = top_A / bottom_A
+    print("Accuracy when k is ["+ str(min_k) +"] is [A = " + str(A) + "]")
+    memorized_accuracies.append([min_k, A])
+    min_k += 1
+
+
 
 while 1:
     try:
